1d667895f37120aa5d1faae64b1e73577e52da12
bemnetSeifu <bemnetseifu@gmail.com>
2024-08-25 20:58:13 +0300

    loading images from .map()

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@
 import Card from "./components/Card"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Data from "./Data"
 
 function App() {
+
+  const cards = Data.map(item => {
+    return (
+        <Card 
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+        />
+    )
+})        
 
 return (
   <div>
     <Navbar />
     <Hero />
-    <Card 
-       img="katie-zaferes.png"
-       rating="5.0"
-       reviewCount={6}
-       country="USA"
-       title="Life Lessons with Katie Zaferes"
-       price={136}
-      />
+    {cards}
   </div>
 )
 }
