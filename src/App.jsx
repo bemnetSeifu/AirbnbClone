4dc7d4ee3f3aa57300e512d23eea7a6b77fa079a
bemnetSeifu <bemnetseifu@gmail.com>
2024-08-25 15:11:09 +0300

    Setting up Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+
+import Card from "./components/Card"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 function App() {
-  
 
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-    </div>
-  )
+return (
+  <div>
+    <Navbar />
+    <Hero />
+    <Card />
+  </div>
+)
 }
 
 export default App
