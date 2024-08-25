ee428d720885e28aef2f59b144a002221703d39c
bemnetSeifu <bemnetseifu@gmail.com>
2024-08-25 21:21:18 +0300

    Sold out badge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 export default function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
+
   return (
     <div className="card">
+           {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={`./${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src="./star.png" className="card--star" />
