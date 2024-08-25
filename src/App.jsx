904c486136639666ae9875aababf550aa6aa18cc
bemnetSeifu <bemnetseifu@gmail.com>
2024-08-25 21:08:40 +0300

    Key prob and fixing card style

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const cards = Data.map(item => {
     return (
         <Card 
+            key={item.id}
             img={item.coverImg}
             rating={item.stats.rating}
             reviewCount={item.stats.reviewCount}
@@ -23,7 +24,9 @@ return (
   <div>
     <Navbar />
     <Hero />
-    {cards}
+    <section className="cards-list">
+                {cards}
+    </section>
   </div>
 )
 }
